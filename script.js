34d550b8608878282f866f7a1a76f67c8ac2026f
SEIF BAHAA <68699464+seifsoss@users.noreply.github.com>
2023-03-01 12:38:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// // You can use JavaScript to add animation to the card
+// const card = document.querySelector(".card");
+// card.addEventListener("mouseenter", () => {
+//   card.style.transform = "scale(1.1)";
+// });
+// card.addEventListener("mouseleave", () => {
+//   card.style.transform = "scale(1)";
+// });
